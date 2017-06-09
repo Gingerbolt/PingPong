c7567b8dfae251f0a324e0c524c09654a6dde480
Steve Spitz <steve@spitz.com>
2017-06-09 12:54:27 -0700

    Add improved front-back division; Remove separate warning field; Improve output function to display once back end loop completes; Add input filter and warning condition;

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,26 @@ $(document).ready(function() {
     var usernumber = parseInt($("input#userinput").val());
     var userstring = usernumber.toString();
     var resultstring = ""
-    for (x = 1; x <= usernumber; x += 1) {
-      if ( x % 15 === 0) {
-        resultstring = resultstring.concat("<li>").concat("Ping-Pong").concat("</li>")
-      }
-      else if (x % 5 === 0) {
-        resultstring = resultstring.concat("<li>").concat("Pong").concat("</li>")
-      }
-      else if (x % 3 === 0) {
-        resultstring = resultstring.concat("<li>").concat("Ping").concat("</li>")
-      }
-      else {
-        resultstring = resultstring.concat("<li>").concat(x).concat("</li>")
-      }
-      $("#result").append(resultstring)
+    if (usernumber % 1 != 0 || usernumber <= 0) {
+      resultstring = "Please enter only positive non-zero integers to play!"
     }
+    else {
+      for (x = 1; x <= usernumber; x += 1) {
+        console.log(x)
+        if ( x % 15 === 0 && x >= 15) {
+        resultstring =   resultstring.concat("<li> ").concat("Ping-Pong").concat(" </li> ")
+        }
+        else if (x % 5 === 0 && x >= 5) {
+        resultstring = resultstring.concat("<li> ").concat("Pong").concat(" </li> ")
+        }
+        else if (x % 3 === 0 && x >= 3) {
+        resultstring = resultstring.concat("<li> ").concat("Ping").concat(" </li> ")
+        }
+        else {
+        resultstring = resultstring.concat("<li> ").concat(x).concat(" </li> ")
+        }
+      }
+    }
+    $("#result").append(resultstring)
   });
 });
