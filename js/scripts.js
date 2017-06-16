74050d011c6abf21cc771830290037f1d2098340
Steve Spitz <steve@spitz.com>
2017-06-16 08:55:47 -0700

    Update javascript with redundant functions to make it unnecessarily harder to read per expectations

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,44 @@
+uselessOperation1 = function() {
+  resultstring = "<li> Ping-Pong </li>"
+};
+uselessOperation2 = function() {
+  resultstring = "<li> Pong </li>"
+};
+uselessOperation3 = function() {
+  resultstring = "<li> Ping </li>"
+};
+uselessOperation4 = function() {
+  resultstring = "<li> " + x + " </li>"
+};
+uselessOperationReject = function() {
+  resultstring = "Please enter only positive non-zero integers to play!"
+}
+uselessOperationPrintout = function () {
+  $("#result").append(resultstring);
+}
+
 $(document).ready(function() {
   $("form#submissionform").submit(function(event) {
     event.preventDefault();
     var usernumber = parseInt($("input#userinput").val());
     var resultstring = ""
     if (usernumber % 1 != 0 || usernumber <= 0) {
-      resultstring = "Please enter only positive non-zero integers to play!"
+      uselessOperationReject();
     }
     else {
       for (x = 1; x <= usernumber; x += 1) {
         if ( x % 15 === 0 && x >= 15) {
-        resultstring =   resultstring.concat("<li> ").concat("Ping-Pong").concat(" </li> ")
-        }
-        else if (x % 5 === 0 && x >= 5) {
-        resultstring = resultstring.concat("<li> ").concat("Pong").concat(" </li> ")
-        }
-        else if (x % 3 === 0 && x >= 3) {
-        resultstring = resultstring.concat("<li> ").concat("Ping").concat(" </li> ")
-        }
-        else {
-        resultstring = resultstring.concat("<li> ").concat(x).concat(" </li> ")
-        }
+        uselessOperation1();
+        } else if (x % 5 === 0) {
+        uselessOperation2();
+        } else if (x % 3 === 0) {
+        uselessOperation3();
+        } else {
+        uselessOperation4();
+      };
       }
     }
-    $("#result").append(resultstring)
+    uselessOperationPrintout();
+
   });
 });
