3f5b044c174aaf9a3032b46da5952f77e3659db0
Steve Spitz <steve@spitz.com>
2017-06-19 20:14:11 -0700

    refactored so that the html was only in a single line

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,23 @@
-uselessOperation1 = function() {
-  resultString = "<li> Ping-Pong </li>"
-};
-uselessOperation2 = function() {
-  resultString = "<li> Pong </li>"
-};
-uselessOperation3 = function() {
-  resultString = "<li> Ping </li>"
-};
-uselessOperation4 = function() {
-  resultString = "<li> " + x + " </li>"
-};
-uselessOperationReject = function() {
-  resultString = "Please enter only positive non-zero integers to play!"
-}
-uselessOperationPrintout = function () {
-  $("#result").append(resultString);
-}
 
 $(document).ready(function() {
   $("form#submissionform").submit(function(event) {
     event.preventDefault();
     var userNumber = parseInt($("input#userinput").val());
-    var resultString = ""
     if (userNumber % 1 != 0 || userNumber <= 0) {
-      uselessOperationReject();
-    }
-    else {
+      resultString = "Please enter only positive non-zero integers to play!"
+    } else {
       for (x = 1; x <= userNumber; x += 1) {
         if ( x % 15 === 0 && x >= 15) {
-        uselessOperation1();
+        resultString = "Ping-Pong";
         } else if (x % 5 === 0) {
-        uselessOperation2();
+        resultString = "Pong"
         } else if (x % 3 === 0) {
-        uselessOperation3();
+        resultString = "Ping"
         } else {
-        uselessOperation4();
+        resultString = x
+        };
+        $("#result").append("<li>" + resultString + "</li>");
       };
-      }
-    }
-    uselessOperationPrintout();
-
+    };
   });
 });
