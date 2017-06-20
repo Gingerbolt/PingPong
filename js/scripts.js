743c52d1be83c7446e1b824a2bd88b53eba3f048
Steve Spitz <steve@spitz.com>
2017-06-19 20:48:21 -0700

    Refactor further confusion remains

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,24 @@ $(document).ready(function() {
   $("form#submissionform").submit(function(event) {
     event.preventDefault();
     var userNumber = parseInt($("input#userinput").val());
+    var finalString = "";
     if (userNumber % 1 != 0 || userNumber <= 0) {
       resultString = "Please enter only positive non-zero integers to play!"
     } else {
-      $("#result").empty()
       for (x = 1; x <= userNumber; x += 1) {
         if ( x % 15 === 0 && x >= 15) {
         resultString = "Ping-Pong";
         } else if (x % 5 === 0) {
-        resultString = "Pong"
+        resultString = "Pong";
         } else if (x % 3 === 0) {
-        resultString = "Ping"
+        resultString = "Ping";
         } else {
-        resultString = x
+        resultString = x;
         };
-        $("#result").append("<li>" + resultString + "</li>");
+        finalString = finalString + resultString + "<br>"
       };
-    };
+    $("#result").empty();
+    $("#result").prepend(finalString);
+  };
   });
 });
