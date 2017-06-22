fa509e7a004ded2db9609e67f5e6bb011ddf62ec
Steve Spitz <steve@spitz.com>
2017-06-21 18:41:11 -0700

    Refactor again upon review; Warning re: negative numbers placed into front end logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,13 @@
 // back end
 pingPongProcess = function(countNumber){
-  if (countNumber % 1 != 0 || countNumber <= 0) {
-    return "Please enter only positive non-zero integers to play!";
+  if ( countNumber % 15 === 0 && countNumber >= 15) {
+  return "Ping-Pong";
+  } else if (countNumber % 5 === 0) {
+  return "Pong";
+  } else if (countNumber % 3 === 0) {
+  return "Ping";
   } else {
-    if ( countNumber % 15 === 0 && countNumber >= 15) {
-    return "Ping-Pong";
-    } else if (countNumber % 5 === 0) {
-    return "Pong";
-    } else if (countNumber % 3 === 0) {
-    return "Ping";
-    } else {
-    return countNumber;
-    };
+  return countNumber;
   };
 };
 // front end
@@ -20,9 +16,14 @@ $(document).ready(function() {
     event.preventDefault();
     var finalString = "";
     var userNumber = parseInt($("input#userinput").val());
-    for (x = 1; x <= userNumber; x++) {
-      finalString = finalString + pingPongProcess(x) + "<br>";
-    }
+
+    if (userNumber % 1 != 0 || userNumber <= 0) {
+      finalString = "Please enter only positive non-zero integers to play!";
+    } else {
+      for (x = 1; x <= userNumber; x++) {
+        finalString = finalString + pingPongProcess(x) + "<br>";
+      };
+    };
     $("#result").empty();
     $("#result").prepend(finalString);
   });
