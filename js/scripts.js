f80479879597a5aef174dbd7319fc50755ed5e30
Steve Spitz <steve@spitz.com>
2017-06-21 18:32:58 -0700

    Refactor code to separate business and UI elements. Separate for loop from business function because it pertains to output.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,29 @@
-
+// back end
+pingPongProcess = function(countNumber){
+  if (countNumber % 1 != 0 || countNumber <= 0) {
+    return "Please enter only positive non-zero integers to play!";
+  } else {
+    if ( countNumber % 15 === 0 && countNumber >= 15) {
+    return "Ping-Pong";
+    } else if (countNumber % 5 === 0) {
+    return "Pong";
+    } else if (countNumber % 3 === 0) {
+    return "Ping";
+    } else {
+    return countNumber;
+    };
+  };
+};
+// front end
 $(document).ready(function() {
   $("form#submissionform").submit(function(event) {
     event.preventDefault();
-    var userNumber = parseInt($("input#userinput").val());
     var finalString = "";
-    if (userNumber % 1 != 0 || userNumber <= 0) {
-      resultString = "Please enter only positive non-zero integers to play!"
-    } else {
-      for (x = 1; x <= userNumber; x += 1) {
-        if ( x % 15 === 0 && x >= 15) {
-        resultString = "Ping-Pong";
-        } else if (x % 5 === 0) {
-        resultString = "Pong";
-        } else if (x % 3 === 0) {
-        resultString = "Ping";
-        } else {
-        resultString = x;
-        };
-        finalString = finalString + resultString + "<br>"
-      };
+    var userNumber = parseInt($("input#userinput").val());
+    for (x = 1; x <= userNumber; x++) {
+      finalString = finalString + pingPongProcess(x) + "<br>";
+    }
     $("#result").empty();
     $("#result").prepend(finalString);
-  };
   });
 });
